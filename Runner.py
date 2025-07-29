c51d968bd8f2842324280f80c30f31b8c8927b5d
Dynix7 <88906210+Dynix7@users.noreply.github.com>
2025-07-29 01:11:55 -0400

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -2,3 +2,21 @@ import atexit
 import argon2
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+import tkinter as tk
+#I'll add a GUI if i feel like it lol
+
+#haha guys im aura farming by making insecure password manager
+
+#Generate salt, Added to the password so hash output is differnt and rainbow table less work
+salt = b''
+
+
+password = input("Enter Password: ")
+
+ph = argon2.PasswordHasher()
+
+
+
+
+
+
