a4f05ca2d5ec971baa5102c221564b4a8251167c
Dynix7 <88906210+Dynix7@users.noreply.github.com>
2025-07-29 17:38:59 -0400

    Added salt option for getKey() and decrypt with key

--- Runner.py
+++ Runner.py
@@ -1,15 +1,14 @@
 import atexit
-
 import argon2
 #https://argon2-cffi.readthedocs.io/en/stable/api.html
 
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import tkinter as tk
+import json
 #I'll add a GUI if i feel like it lol
 
 #haha guys im aura farming by making insecure password manager
-
 
 
 password = input("Enter Password: ")
@@ -17,7 +16,7 @@ password = input("Enter Password: ")
 
 #HM OK CHAT ACTUALLY IT GIVES THE BASE64 AND NOT ACTUAL BYTES SO CHAT WAIT A SEC
 #This generates the 32 byte key from the password using argon2
-def getKey(password=None):
+def getKey(password=None, salt=None):
 
     #Prompts for a password if one isn't given
     if password == None:
@@ -26,8 +25,10 @@ def getKey(password=None):
     #Turns the string to bytes
     password = password.encode()
 
+
     #Generate random 16 byte salt
-    salt = get_random_bytes(16)
+    if salt == None:
+        salt = get_random_bytes(16)
 
     #Just using default parameters for time, memory, and parallelism listed on documentation with ID type being the most secure
     #  this function DIRECTLY GIVES THE 32bytes and not base64encoded
@@ -38,16 +39,22 @@ def getKey(password=None):
 
     return key, salt
 
-key, salt = getKey(password=password)
 
-print(key, salt)
+
 
 def encrypt(key, message):
+
     #Generates the Nonce/IV (basically random bytes to ensure every encryption of the same data is different, kinda like the salt)
     nonce = get_random_bytes(16)
 
+    #Convert to json then to bytes
+    message = json.dumps(message)
+    message = message.encode()
+
+
     #sets up the cipher with the key, aes mode, and nonce
     cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+
 
     #generates the ciphertext and a tag (verification basically)
     ciphertext, tag = cipher.encrypt_and_digest(message)
@@ -55,6 +62,18 @@ def encrypt(key, message):
     return ciphertext, tag, nonce
 
 
+def decrypt(ciphertext, tag, nonce, key):
 
-print(encrypt(key, b'amongus'))
+    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+    message = cipher.decrypt_and_verify(ciphertext, tag)
+
+    message = message.decode()
+    message = json.loads(message)
+
+    return message
+
+    
+    
+
+
 
